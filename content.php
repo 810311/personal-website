c31992e41fffdfbd93973e76e1cdafc1bfe2aab0
Sergiy Prygara <sergiy.prygara@gmail.com>
2025-11-05 22:44:56 -0500

    Add files via upload

---
+++ content.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template part for displaying blog posts via index.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package my_wp_theme
+ */
+
+?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <header class="entry-header">
+            <?php
+                if ( is_singular() ) :
+                  the_title( '<h1 class="entry-title">', '</h1>' );
+                else :
+                  the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                endif;
+
+                if ( 'post' === get_post_type() ) :
+            ?>
+              <div class="entry-meta">
+                <?php echo get_the_date();?>
+              </div><!-- .entry-meta -->
+            <?php endif; ?>
+        </header><!-- .entry-header -->
+
+	<?php my_wp_theme_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php the_excerpt();?>
+	</div><!-- .entry-content -->
+
+   <div class="read-more">
+      <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
+        read more
+      </a>
+   </div>
+
+</article><!-- #post-<?php the_ID(); ?> -->
